18c892f74c03dba63128a23f054502d1069e7215
mhaseeb <haseebahmad7862@gmail.com>
2025-12-11 11:43:47 +0500

    server setup

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express=require('express');
 const cors=require('cors');
-
+const path = require('path');
+const fs = require('fs');
 const app=express();
-
+app.use(cors({origin:"*"}));
+app.use(express.static('build'));
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.listen(3001,()=>console.log('server started...at 3001'));
